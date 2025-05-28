1976958dcdc1432cc7cee2ff0fb7e44144a030a0
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-05-28 21:57:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
 const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+"https://picsum.photos/id/237/200/300",
+  "https://picsum.photos/id/238/200/300",
+  "https://picsum.photos/id/239/200/300"
 ];
 function downloadImage(url) {
       return new Promise((resolve, reject) => {
